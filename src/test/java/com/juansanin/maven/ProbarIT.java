a7782b6b41a80e5067d443cda725f7053fca686a
Juan Sanin <jsanin@gmail.com>
2015-05-05 23:36:06 -0500

    primera versión

---
+++ src/test/java/com/juansanin/maven/ProbarIT.java
@@ -0,0 +1,38 @@
+package com.juansanin.maven;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class ProbarIT 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public ProbarIT( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( ProbarIT.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertEquals("aaa", "AAA");
+    }
+}
